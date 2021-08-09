765feed2e0b6e413389bd7ec167bb8e298ba2cf2
Daniela Ceron <cafedeollaparatodos@gmail.com>
2021-08-09 14:17:13 -0500

    se subio archivo prueba

--- ejercicio2.R
+++ ejercicio2.R
@@ -4,3 +4,7 @@ usethis::use_github()
 install.packages("postcards")
 library(postcards)
 postcards::create_postcard(template = "jolla")
+
+gert::git_commit("se subio archivo prueba")
+
+gert::git_push()
